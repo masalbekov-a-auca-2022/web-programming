2b1d22274b9832eba411b2bb8c6d37d0f4709741
masalbekov-a-auca-2022 <113011880+masalbekov-a-auca-2022@users.noreply.github.com>
2025-01-30 17:28:42 +0600

    Add files via upload

---
+++ Lab7/main.py
@@ -0,0 +1,15 @@
+for i in range(11):
+    print(i)
+
+i = 0
+while i <= 10:
+    print(i)
+    i+=1
+
+for i in "Python":
+    print(i)
+i = 2
+while i <=10:
+    if i%2 == 0:
+        print(i)
+    i+=1
