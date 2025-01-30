d456100f4417a4d3d5a85a7684b7371c16847e95
masalbekov-a-auca-2022 <113011880+masalbekov-a-auca-2022@users.noreply.github.com>
2025-01-30 11:41:06 +0600

    Add files via upload

---
+++ Lab5/lab-exercise-1.py
@@ -0,0 +1,20 @@
+import math
+
+a = "Aziret"
+
+b = 10
+c = 5
+
+print(c+b)
+print(c-b)
+print(c*b)
+print(c/b)
+
+print(c, b)
+b, c = c, b
+
+print(c, b)
+
+x = math.pi
+
+print(x*(5**2))
---
+++ Lab5/lab-exercise-2.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello {name}, you are {age} years old.")
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(a+b+c)
+print((a+b+c)/3)
+print(a*b*c)
