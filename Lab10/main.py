a66b7bab3bdb31869f5a7e036d1b084a40ab6530
masalbekov-a-auca-2022 <113011880+masalbekov-a-auca-2022@users.noreply.github.com>
2025-03-03 10:47:31 +0600

    Lab10

---
+++ Lab10/main.py
@@ -0,0 +1,27 @@
+import math
+from math import sqrt
+import math as m
+from math import *
+
+print(math.sqrt(5))
+print(sqrt(5))
+print(m.pi)
+print(cos(0))
+
+r = 5
+area = m.pi * r**2
+print(area)
+
+import random
+
+names = ["theGod", "BlackOne", "GreedOfH"]
+w = random.choice(names)
+print(w)
+
+from datetime import datetime, timedelta
+
+now = datetime.now()
+print(now)
+
+future = now + timedelta(minutes=50)
+print(future)
